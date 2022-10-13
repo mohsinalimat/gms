1bc76a67255d315ba3be3590b773d4f7288b5081
Lucky-Tsuma <tsumahmumba@gmail.com>
2022-10-13 22:30:55 +0300

    Initial commit

---
+++ gms/gms/doctype/grant_application/grant_application.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Navari Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+class GrantApplication(WebsiteGenerator):
+	pass
